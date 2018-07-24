be18d8117b4813ecb4520ef43df0a36fe52c0543
Jonas Schwammberger <jonas.schwammberger@fhnw.ch>
2018-07-24 03:32:26 +0200

    before cs deletion

--- eval/05.results.r
+++ eval/05.results.r
@@ -19,14 +19,6 @@ read <- function(images) {
   return(images.list)
 }
 
-logtrans <- function(x) {
-  s <- sign(x)
-  zero <- x[x == 0]
-  x.abs <- abs(x)
-  x2 <- s*abs(log10(x.abs))
-  x2[zero] = 0
-}
-
 #write png images on same intensity profile except raw images
 writePNGs <- function(images.list, names, plotLen) {
   max = 0
@@ -55,6 +47,7 @@ writePNGs <- function(images.list, names, plotLen) {
   }
 }
 
+
 calcline <- function(matrix, p0, p1, length.out=100) {
   a <- p1 - p0
   x0 <- p0 + a*((-p0[2]+1)/ a[2])
@@ -68,7 +61,7 @@ calcline <- function(matrix, p0, p1, length.out=100) {
   
   a <- y0 - x0
   line <-rep(c(0),length.out=length.out)
-  i =1
+  i = 1
   for(t in seq(0, 1, length.out = length.out)) {
     pt <- x0+a*t
     pt.ceil <- ceiling(pt)
@@ -80,11 +73,37 @@ calcline <- function(matrix, p0, p1, length.out=100) {
     xt0 <- pt.factor[1] * matrix[pt.floor[1], pt.floor[2]] + (1-pt.factor[1]) * matrix[pt.ceil[1],pt.floor[2]]
     x <- pt.factor[2] * xt0 + (1-pt.factor[2])*xt1
     line[i] <- x
+    
     i = i+ 1
   }
   
   return(line)
 }
+
+
+calcRowLine <- function(matrix, p0,p1,length.out=100) {
+  a <- p1 - p0
+  x0 <- p0 + a*((-p0[2]+1)/ a[2])
+  if(x0[1] > nrow(matrix)) {
+    x0 <- p0 + a*((nrow(matrix)-p0[1])/ a[1])
+  }
+  y0 <- p0 + a*((-p0[1]+1)/ a[1])
+  if(y0[2] > ncol(matrix)) {
+    y0 <- p0 + a*((ncol(matrix)-p0[2])/ a[2])
+  }
+  
+  a <- y0 - x0
+  x_names <-rep(c(0),length.out=length.out)
+  i = 1
+  for(t in seq(0, 1, length.out = length.out)) {
+    pt <- x0+a*t
+    x_names[i] <- pt[1]
+    i = i + 1
+  }
+  
+  return(x_names)
+}
+
 
 plotline <- function(mod.list, files1, files2) {
   p0 <- c(92,29)
@@ -97,6 +116,7 @@ plotline <- function(mod.list, files1, files2) {
   i1 <- 0
   for (file in files1) {
     d = data.matrix(mod.list[[file]])
+    print(file)
     line <- calcline(d, p0, p1, interpolation.length)
     start <- i1 + 1
     stop <- i1 + interpolation.length
@@ -105,7 +125,7 @@ plotline <- function(mod.list, files1, files2) {
     df1$image[start:stop] = file
     i1 = i1 + interpolation.length
   }
-  
+  print("")
   df2 <- data.frame(matrix(ncol=3, nrow=length(files2)*interpolation.length))
   colnames(df2) <- c("index", "value","image")
   i2 <- 0
@@ -120,16 +140,42 @@ plotline <- function(mod.list, files1, files2) {
     df2$image[start:stop] = file
     i2 = i2 + interpolation.length
   }
-  bla <- df1$value
   
-  df1$value <- bla
-  p <- ggplot(data = df1, aes(x=df1$index, y=df1$value, colour=df1$image))
-  p + geom_line()
-  
-  p <- ggplot(data = df2, aes(x=df2$index, y=df2$value, colour=df2$image)) 
-  p + geom_line()
+  print(ggplot(data = df1, aes(x=df1$index, y=df1$value, colour=df1$image)) + geom_line())
+  print(ggplot(data = df2, aes(x=df2$index, y=df2$value, colour=df2$image)) + geom_line())
 }
 
+
+writeExport <- function(mod.list, files1, files2) {
+  p0 <- c(92,29)
+  #p1 <- c(29,85)
+  p1 <- c(46,59)
+  interpolation.length <- 500
+  
+  df1 <- data.frame(matrix(ncol=length(files1), nrow=interpolation.length))
+  colnames(df1) <- files1
+  row.names <- rev(calcRowLine(data.matrix(mod.list[["raw_model"]]), p0, p1, interpolation.length))
+  rownames(df1) <- row.names
+  for (file in files1) {
+    print(file)
+    d = data.matrix(mod.list[[file]])
+    line  <- rev(calcline(d, p0, p1, interpolation.length))
+    df1[1:interpolation.length, file] = line
+  }
+  
+  df2 <- data.frame(matrix(ncol=length(files2), nrow=interpolation.length))
+  colnames(df2) <- files2
+  rownames(df2) <- row.names
+  for (file in files2) {
+    print(file)
+    d = data.matrix(mod.list[[file]])
+    line <- rev(calcline(d, p0, p1, interpolation.length))
+    df2[1:interpolation.length, file] = line
+  }
+  
+  write.table(df1, file="./data/part1.csv", sep=";")
+  write.table(df2, file="./data/part2.csv", sep=";")
+}
 
 inFolder = "../../p7-cs/results/supernova/csv/"
 outFolder = "../chapters/05.results/g55/"
@@ -156,13 +202,11 @@ mod.list[["raw_model"]] <- data.matrix(img.list[["raw_image"]])
 writePNGs(res.list,res.names, 300)
 writePNGs(mod.list,mod.names, 200)
 
+files1 <- c("raw_model", "clean_model", "positive_deconv_model" ,"L1_model", "L2_model")
+files2 <- c( "clean_model", "L2_model","TV_model", "starlets3_model")
+#writeExport(mod.list, files1, files2)
 
 library(ggplot2)
 library(scales)
 
-
-
-files1 <- c("raw_model", "clean_model", "positive_deconv_model" ,"L1_model", "L2_model")
-files2 <- c("raw_model", "clean_model", "TV_model", "haar_model", "starlets3_model")
 plotline(mod.list,files1, files2)
-
